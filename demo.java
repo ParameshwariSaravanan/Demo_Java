527eb84e3b36b22cd8902ff94d2d7a9c6ed2b5e6
ParameshwariSaravanan <parameshwarias06@gmail.com>
2024-11-08 20:42:30 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -4,5 +4,6 @@ public class demo{
         System.out.println ("Hello to Sweta from demo java");
          System.out.println ("Hello to Jayant from demo java");
                System.out.println ("Hello to Takshal from demo java");
+                    System.out.println ("Hello to All from demo java");
     }
 }
